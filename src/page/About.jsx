f823a63cbb0926205d7f74dd6e3cbf557b6c7a48
amynhha <142844316+amynhha@users.noreply.github.com>
2026-07-22 10:59:33 -0500

    Update About.jsx
    changing about to aboutUs

--- src/page/About.jsx
+++ src/page/About.jsx
@@ -1,14 +1,14 @@
 import "../App.css";
 import NavBar from "../component/NavBar";
 import Hero from "../component/Hero";
-import about from "../assets/about.jpg";
+import aboutUs from "../assets/aboutUs.png";
 
 export default function About() {
     return (
         <div>
             <NavBar />
             <Hero
-                img={about}
+                img={aboutUs}
                 title="Move Your Freight With Confidence"
                 text="At 9Volt Logistics, we believe transportation should be simple, dependable, and built on strong relationships. Our team is dedicated to connecting shippers with trusted carriers while delivering exceptional service every step of the way."
                 subtext={[
